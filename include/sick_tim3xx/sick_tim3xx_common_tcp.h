fbe3de8dfeba3db6b6c3069490d48f978d999cf0
Christian Dornhege <dornhege@informatik.uni-freiburg.de>
2013-11-15 17:13:50 +0100

    prepare option for TCP on sick_tim551_2050001

--- include/sick_tim3xx/sick_tim3xx_common_tcp.h
+++ include/sick_tim3xx/sick_tim3xx_common_tcp.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2013, Osnabrück University
+ * Copyright (C) 2013, Freiburg University
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of Osnabrück University nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,35 +26,48 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- *  Created on: 14.11.2013
+ *  Created on: 15.11.2013
  *
- *      Author:
- *         Martin Günther <mguenthe@uos.de>
- *
+ *      Authors:
+ *         Christian Dornhege <c.dornhege@googlemail.com>
  */
 
-#include <sick_tim3xx/sick_tim3xx_common_usb.h>
-#include <sick_tim3xx/sick_tim3xx_common_tcp.h>
-#include <sick_tim3xx/sick_tim551_2050001_parser.h>
+#ifndef SICK_TIM3XX_COMMON_TCP_H
+#define SICK_TIM3XX_COMMON_TCP_H
 
-int main(int argc, char **argv)
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sick_tim3xx/sick_tim3xx_common.h>
+
+namespace sick_tim3xx
 {
-  ros::init(argc, argv, "sick_tim551_2050001");
 
-  sick_tim3xx::SickTim5512050001Parser* parser = new sick_tim3xx::SickTim5512050001Parser();
-  sick_tim3xx::SickTim3xxCommon* s = NULL;
-  if(argc > 1 && strcmp(argv[1], "-tcp") == 0)
-      s = new sick_tim3xx::SickTim3xxCommonTcp(parser);
-  else
-      s = new sick_tim3xx::SickTim3xxCommonUsb(parser);
+class SickTim3xxCommonTcp : public SickTim3xxCommon
+{
+public:
+  SickTim3xxCommonTcp(AbstractParser* parser);
+  virtual ~SickTim3xxCommonTcp();
 
-  int result = s->init();
-  while (ros::ok() && (result == EXIT_SUCCESS))
-  {
-    ros::spinOnce();
-    result = s->loopOnce();
-  }
+protected:
+  virtual int init_device();
+  virtual int close_device();
 
-  delete s;
-  return result;
-}
+  /// Send a SOPAS command to the device and print out the response to the console.
+  virtual int sendSOPASCommand(const char* request);
+
+  /// Read a datagram from the device.
+  /**
+   * \param [in] receiveBuffer data buffer to fill
+   * \param [in] bufferSize max data size to write to buffer (result should be 0 terminated)
+   * \param [out] actual_length the actual amount of data written
+   */
+  virtual int get_datagram(unsigned char* receiveBuffer, int bufferSize, int* actual_length);
+
+private:
+};
+
+} /* namespace sick_tim3xx */
+#endif /* SICK_TIM3XX_COMMON_TCP_H */
+
