93ff8dcab77cf6b2dea19c82af06dae27c37a66c
Christian Dornhege <dornhege@informatik.uni-freiburg.de>
2013-11-18 17:58:49 +0100

    add TCP connection

--- src/sick_tim3xx_common_tcp.cpp
+++ src/sick_tim3xx_common_tcp.cpp
@@ -32,13 +32,22 @@
  *         Christian Dornhege <c.dornhege@googlemail.com>
  */
 
+#include <sys/socket.h>
+#include <strings.h>
+#include <netdb.h>
 #include <sick_tim3xx/sick_tim3xx_common_tcp.h>
 
 namespace sick_tim3xx
 {
 
-SickTim3xxCommonTcp::SickTim3xxCommonTcp(AbstractParser* parser) : SickTim3xxCommon(parser)
+SickTim3xxCommonTcp::SickTim3xxCommonTcp(AbstractParser* parser) : SickTim3xxCommon(parser),
+    socket_fd_(-1)
 {
+    ros::NodeHandle nhPriv("~");
+    if(!nhPriv.getParam("hostname", hostname_)) {
+        ROS_FATAL("Parameter ~hostname not set. Don't know where to connect.");
+        exit(1);    // nasty
+    }
 }
 
 SickTim3xxCommonTcp::~SickTim3xxCommonTcp()
@@ -47,10 +56,49 @@ SickTim3xxCommonTcp::~SickTim3xxCommonTcp()
   close_device();
 }
 
+int SickTim3xxCommonTcp::init_device()
+{
+    socket_fd_ = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_fd_ == -1) {
+        ROS_FATAL("Could not open socket: %d", errno);
+        return EXIT_FAILURE;
+    }
+
+    struct addrinfo hints;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+    hints.ai_protocol = 0;
+
+    struct addrinfo* result;
+    int res = getaddrinfo(hostname_.c_str(), "2112", &hints, &result);
+    if(res != 0) {
+        ROS_FATAL("Could not resolve host: ... (%d)", res);
+        return EXIT_FAILURE;
+    }
+
+    // try to connect to any of the returned host infos
+    struct addrinfo* cur_addr;
+    for(cur_addr = result; cur_addr != NULL; cur_addr = cur_addr->ai_next) {
+        if(connect(socket_fd_, cur_addr->ai_addr, cur_addr->ai_addrlen) != -1)
+            break;
+    }
+
+    freeaddrinfo(result);
+
+    if(cur_addr == NULL) {
+        ROS_FATAL("Could not connect to host %s", hostname_.c_str());
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
+
 int SickTim3xxCommonTcp::close_device()
 {
-  int result = 0;
-  return result;
+    if(socket_fd_ != -1)
+        return close(socket_fd_);
+    return 0;
 }
 
 /**
@@ -58,26 +106,60 @@ int SickTim3xxCommonTcp::close_device()
  */
 int SickTim3xxCommonTcp::sendSOPASCommand(const char* request)
 {
-  int result = 0;
-  unsigned char receiveBuffer[65536];
+    if(socket_fd_ == -1) {
+        ROS_ERROR("sendSOPASCommand: socket_fd_ not open");
+        return EXIT_FAILURE;
+    }
 
-  /*
-   * Write a SOPAS variable read request to the device.
-   */
+    /*
+     * Write a SOPAS variable read request to the device.
+     */
+    ssize_t bytesWritten = write(socket_fd_, request, strlen(request));
+    if(bytesWritten != (ssize_t)strlen(request)) {
+        ROS_ERROR("write error for command: %s", request);
+        return EXIT_FAILURE;
+    }
 
-  return result;
-}
+    unsigned char receiveBuffer[65536];
+    ssize_t bytesRead = read(socket_fd_, receiveBuffer, 65536 - 1);
+    if(bytesRead < 0) {
+        // FIXME might need a select
+        ROS_ERROR("read error after command: %s (%d)", request, errno);
+        return EXIT_FAILURE;
+    }
+    receiveBuffer[bytesRead] = 0;
+    ROS_DEBUG("TCP - Read data...  %s", receiveBuffer);
 
-int SickTim3xxCommonTcp::init_device()
-{
+    // FIXME: need to puzzle together messages from stream???
+    // see below
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 int SickTim3xxCommonTcp::get_datagram(unsigned char* receiveBuffer, int bufferSize, int* actual_length)
 {
+    if(socket_fd_ == -1) {
+        ROS_ERROR("get_datagram: socket_fd_ not open");
+        return EXIT_FAILURE;
+    }
 
-  return EXIT_SUCCESS;
+    /*
+     * Write a SOPAS variable read request to the device.
+     */
+    ssize_t bytesRead = read(socket_fd_, receiveBuffer, bufferSize - 1);
+    if(bytesRead < 0) {
+        // FIXME might need a select
+        ROS_ERROR("get_datagram: Read error"); 
+        return EXIT_FAILURE;
+    }
+    receiveBuffer[bytesRead] = 0;
+    ROS_DEBUG("TCP - Read data...  %s", receiveBuffer);
+    *actual_length = bytesRead + 1; // include \0
+
+    // FIXME Ideally need to puzzle together messages from stream.
+    // Currently working on LAN as the scanner sends 1 packet/scan.
+
+    return EXIT_SUCCESS;
 }
 
 } /* namespace sick_tim3xx */
