6e32cb47badf72c67d0f3004aac46fdf75c7004e
Sebastian Pütz <spuetz@uos.de>
2017-11-29 16:55:20 +0100

    sick mrs1000 driver

--- include/sick_tim/sick_mrs1000_parser.h
+++ include/sick_tim/sick_mrs1000_parser.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2013, Osnabrück University
+ * Copyright (C) 2017, Osnabrück University
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of Osnabrück University nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,39 +26,45 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- *  Created on: 15.08.2013
- *
- *      Author: Martin Günther <mguenthe@uos.de>
+ *      Author:
+ *         Sebastian Pütz <spuetz@uos.de>
  *
  */
 
-#ifndef ABSTRACT_PARSER_H_
-#define ABSTRACT_PARSER_H_
 
-#include <sick_tim/SickTimConfig.h>
-#include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/PointCloud2.h>
+#ifndef SICK_MRS1000_PARSER_H_
+#define SICK_MRS1000_PARSER_H_
+
+#include "sick_tim/scan_and_cloud_parser.h"
+#include <sensor_msgs/point_cloud2_iterator.h>
 
 namespace sick_tim
 {
 
-enum ExitCode
+class SickMRS1000Parser : public ScanAndCloudParser
 {
-    ExitSuccess = 0
-    , ExitError = 1    // non-fatal error, retry
-    , ExitFatal = 2    // fatal error, exit
-};
-
-class AbstractParser
-{
-public:
-  AbstractParser();
-  virtual ~AbstractParser();
+ public:
+  SickMRS1000Parser();
+  virtual ~SickMRS1000Parser();
 
   virtual int parse_datagram(char* datagram, size_t datagram_length, SickTimConfig &config,
-                             sensor_msgs::LaserScan &msg) = 0;
+                             sensor_msgs::LaserScan &scan, sensor_msgs::PointCloud2& cloud);
 
+  void set_range_min(float min);
+  void set_range_max(float max);
+  void set_time_increment(float time);
+
+  sensor_msgs::PointCloud2 cloud_;
+  sensor_msgs::PointCloud2Modifier modifier_;
+  sensor_msgs::PointCloud2Iterator<float> x_iter, y_iter, z_iter;
+
+ private:
+  size_t point_counter_;
+  int layer_count_;
+
+  float override_range_min_, override_range_max_;
+  float override_time_increment_;
 };
 
 } /* namespace sick_tim */
-#endif /* ABSTRACT_PARSER_H_ */
+#endif /* SICK_MRS1000_PARSER_H_ */
--- src/sick_mrs1000.cpp
+++ src/sick_mrs1000.cpp
@@ -1,10 +1,10 @@
 /*
- * Copyright (C) 2013, Osnabrück University
+ * Copyright (C) 2017, Osnabrück University
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
  *     * Neither the name of Osnabrück University nor the names of its
  *       contributors may be used to endorse or promote products derived from
  *       this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,39 +26,69 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  *
- *  Created on: 15.08.2013
- *
- *      Author: Martin Günther <mguenthe@uos.de>
+ *      Author:
+ *         Sebastian Pütz <spuetz@uos.de>
  *
  */
 
-#ifndef ABSTRACT_PARSER_H_
-#define ABSTRACT_PARSER_H_
+#include <sick_tim/sick_mrs1000_communication.h>
+#include <sick_tim/sick_mrs1000_parser.h>
 
-#include <sick_tim/SickTimConfig.h>
-#include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/PointCloud2.h>
-
-namespace sick_tim
+int main(int argc, char **argv)
 {
+  ros::init(argc, argv, "sick_mrs_1000");
+  ros::NodeHandle nhPriv("~");
 
-enum ExitCode
-{
-    ExitSuccess = 0
-    , ExitError = 1    // non-fatal error, retry
-    , ExitFatal = 2    // fatal error, exit
-};
+  // check for TCP - use if ~hostname is set.
+  bool useTCP = false;
+  std::string hostname;
+  if(!nhPriv.getParam("hostname", hostname))
+  {
+    ROS_FATAL_STREAM("No hostname given!");
+    return sick_tim::ExitError;
+  }
+  std::string port;
+  nhPriv.param<std::string>("port", port, "2112");
 
-class AbstractParser
-{
-public:
-  AbstractParser();
-  virtual ~AbstractParser();
+  sick_tim::SickMRS1000Parser* parser = new sick_tim::SickMRS1000Parser();
 
-  virtual int parse_datagram(char* datagram, size_t datagram_length, SickTimConfig &config,
-                             sensor_msgs::LaserScan &msg) = 0;
+  double param;
+  if (nhPriv.getParam("range_min", param))
+  {
+    parser->set_range_min(param);
+  }
+  if (nhPriv.getParam("range_max", param))
+  {
+    parser->set_range_max(param);
+  }
+  if (nhPriv.getParam("time_increment", param))
+  {
+    parser->set_time_increment(param);
+  }
 
-};
+  int timelimit;
+  nhPriv.param("timelimit", timelimit, 5);
 
-} /* namespace sick_tim */
-#endif /* ABSTRACT_PARSER_H_ */
+  sick_tim::SickTimCommon* s = NULL;
+
+  int result = sick_tim::ExitError;
+  while (ros::ok())
+  {
+    // Atempt to connect/reconnect
+    delete s;
+    s = new sick_tim::SickMrs1000Communication(hostname, port, timelimit, parser);
+    result = s->init();
+
+    while(ros::ok() && (result == sick_tim::ExitSuccess)){
+      ros::spinOnce();
+      result = s->loopOnce();
+    }
+
+    if (result == sick_tim::ExitFatal)
+      return result;
+  }
+
+  delete s;
+  delete parser;
+  return result;
+}
